26a333ea79aacea1c8cf70182875533a47abeceb
Jannat-358 <jannatsanjida10@gmail.com>
2025-01-31 12:46:13 +0600

    Update Task_6.py

--- AL-316/CLP-1/Task_6.py
+++ AL-316/CLP-1/Task_6.py
@@ -1,1 +1,15 @@
+n = int(input('Enter the number: '))
+first = 0
+second = 1
+
+print('Series:', end=' ')
+
+print(first, end=' ')
+print(second, end=' ')
+
+for i in range(3, n + 1):
+    next_num = first + second
+    print(next_num, end=' ')
+    first = second
+    second = next_num
 
