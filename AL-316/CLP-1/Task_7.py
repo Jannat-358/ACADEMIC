9c5c4835217d2e9537c2cae4772bcafec352615e
Jannat-358 <jannatsanjida10@gmail.com>
2025-01-31 12:46:40 +0600

    Update Task_7.py

--- AL-316/CLP-1/Task_7.py
+++ AL-316/CLP-1/Task_7.py
@@ -1,1 +1,8 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if num1 > num2:
+    print(f"The largest number is:" + str(num1))
+else:
+    print("The largest number is:" + str(num2))
 
