1b79e248171f89c124e67dc422298154e3d67fc8
Jannat-358 <jannatsanjida10@gmail.com>
2025-01-31 12:45:08 +0600

    Update Task_4.py

--- AL-316/CLP-1/Task_4.py
+++ AL-316/CLP-1/Task_4.py
@@ -1,1 +1,15 @@
+numbers = {23, 45, 12, 89, 5, 38}
+
+
+numbers_list = list(numbers)
+n = len(numbers_list)
+for i in range(n):
+    for j in range(0, n-i-1):
+        if numbers_list[j] > numbers_list[j+1]:
+
+            numbers_list[j], numbers_list[j+1] = numbers_list[j+1], numbers_list[j]
+
+second_highest = numbers_list[-3]
+
+print(f"Second highest number: {second_highest}")
 
